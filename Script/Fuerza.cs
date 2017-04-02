9885f08398c040aa1ffe66389cffb146fe7ba3bf
ReaperDream <henryt.o@hotmail.com>
2017-04-02 17:54:05 -0600

    BrickBreaker

---
+++ Script/Fuerza.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuerza : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float ballForce;
+    bool gameStarted = false;
+    private Vector2 InitialLocation;
+	// Use this for initialization
+	void Start () {
+        InitialLocation = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && gameStarted == false)
+        {
+            BallReset();
+            rb.AddForce(new Vector2(ballForce, ballForce));
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Reiniciar"))
+        {
+            transform.position = InitialLocation;
+            BallReset();
+        }
+    }
+    void BallReset()
+    {
+        transform.SetParent(null);
+        rb.isKinematic = false;
+        gameStarted = true;
+    }
+    
+}
---
+++ Script/Mover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+   
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            var vel = rb.velocity;
+            vel.x = -speed;
+            rb.velocity = vel;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            var vel = rb.velocity;
+            vel.x = speed;
+            rb.velocity = vel;
+        }
+        else if (!Input.anyKey)
+        {
+            var vel = rb.velocity;
+            vel.x = 0.0f;
+            rb.velocity = vel;
+
+        }
+        
+    }
+    
+}
